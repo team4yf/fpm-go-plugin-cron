9cfdcc0ff2150cf693106e500f62927f22959c71
yfsoftcom <yfsoftcom@126.com>
2020-09-09 16:54:13 +0800

    add handler to the AFTER_INIT

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -23,74 +23,77 @@ func init() {
 		Name: "fpm-plugin-cron",
 		V:    "0.0.1",
 		Handler: func(fpmApp *fpm.Fpm) {
-			// fetch config
-			cronSetting := cronConfig{
-				Store: "memory",
-			}
-			if fpmApp.HasConfig("cron") {
-				if err := fpmApp.FetchConfig("cron", &cronSetting); err != nil {
-					panic(err)
+			fpmApp.AddHook("AFTER_INIT", func(_ *fpm.Fpm) {
+				// fetch config
+				cronSetting := cronConfig{
+					Store: "memory",
 				}
-			}
+				if fpmApp.HasConfig("cron") {
+					if err := fpmApp.FetchConfig("cron", &cronSetting); err != nil {
+						panic(err)
+					}
+				}
 
-			jobService = job.NewSimpleJobService(repo.NewRepo(cronSetting.Store))
-			jobService.Init()
-			jobService.Start()
-			fpmApp.AddBizModule("job", &fpm.BizModule{
-				"list": func(param *fpm.BizParam) (data interface{}, err error) {
-					data, err = jobService.List()
-					return
-				},
-				"add": func(param *fpm.BizParam) (data interface{}, err error) {
-					var req model.Job
-					if err = param.Convert(&req); err != nil {
+				jobService = job.NewSimpleJobService(repo.NewRepo(cronSetting.Store))
+				jobService.Init()
+				jobService.Start()
+				fpmApp.AddBizModule("job", &fpm.BizModule{
+					"list": func(param *fpm.BizParam) (data interface{}, err error) {
+						data, err = jobService.List()
 						return
-					}
-					err = jobService.Add(&req)
-					data = 1
-					return
-				},
-				"execute": func(param *fpm.BizParam) (data interface{}, err error) {
-					var req codeReq
-					if err = param.Convert(&req); err != nil {
+					},
+					"add": func(param *fpm.BizParam) (data interface{}, err error) {
+						var req model.Job
+						if err = param.Convert(&req); err != nil {
+							return
+						}
+						err = jobService.Add(&req)
+						data = 1
 						return
-					}
-					data, err = jobService.Execute(req.Code)
-					return
-				},
-				"restart": func(param *fpm.BizParam) (data interface{}, err error) {
-					var req codeReq
-					if err = param.Convert(&req); err != nil {
+					},
+					"execute": func(param *fpm.BizParam) (data interface{}, err error) {
+						var req codeReq
+						if err = param.Convert(&req); err != nil {
+							return
+						}
+						data, err = jobService.Execute(req.Code)
 						return
-					}
-					err = jobService.Restart(req.Code)
-					data = 1
-					return
-				},
-				"pause": func(param *fpm.BizParam) (data interface{}, err error) {
-					var req codeReq
-					if err = param.Convert(&req); err != nil {
+					},
+					"restart": func(param *fpm.BizParam) (data interface{}, err error) {
+						var req codeReq
+						if err = param.Convert(&req); err != nil {
+							return
+						}
+						err = jobService.Restart(req.Code)
+						data = 1
 						return
-					}
-					err = jobService.Pause(req.Code)
-					data = 1
-					return
-				},
-				"tasks": func(param *fpm.BizParam) (data interface{}, err error) {
-					var req codeReq
-					if err = param.Convert(&req); err != nil {
+					},
+					"pause": func(param *fpm.BizParam) (data interface{}, err error) {
+						var req codeReq
+						if err = param.Convert(&req); err != nil {
+							return
+						}
+						err = jobService.Pause(req.Code)
+						data = 1
 						return
-					}
-					list, total, err := jobService.Tasks(req.Code, req.Skip, req.Limit)
-					return map[string]interface{}{
-						"row":   list,
-						"total": total,
-					}, err
-				},
-			})
-			fpmApp.Subscribe("#job/done", func(topic string, payload interface{}) {
-				fpmApp.Logger.Debugf("t %s, p %v", topic, payload)
-			})
+					},
+					"tasks": func(param *fpm.BizParam) (data interface{}, err error) {
+						var req codeReq
+						if err = param.Convert(&req); err != nil {
+							return
+						}
+						list, total, err := jobService.Tasks(req.Code, req.Skip, req.Limit)
+						return map[string]interface{}{
+							"row":   list,
+							"total": total,
+						}, err
+					},
+				})
+			}, 1)
+
+			// fpmApp.Subscribe("#job/done", func(topic string, payload interface{}) {
+			// 	fpmApp.Logger.Debugf("t %s, p %v", topic, payload)
+			// })
 		},
 	})
 }
